77122e253c6ae772058bb29cdf952bad6d75ac15
AlenaKatt <katt.elena@yandex.ru>
2021-03-25 13:17:46 +0300

    Больше деталей

---
+++ js/util.js
@@ -0,0 +1,21 @@
+let randomInteger = function (min, max) {
+  if (min >= 0 && max >= 0) {
+    return parseInt((Math.random() * (max - min) + min), 10);
+  }
+  if (max < min || max === min) {
+    return false;
+  }
+}
+
+let checkLength = function (lineToCheck, maxLenth) {
+  if (lineToCheck.length <= maxLenth) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+// Временно вызываю функцию
+checkLength('строка',6);
+
+export {randomInteger};

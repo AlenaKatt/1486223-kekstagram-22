6ac96c3091f505827849abaa7c43396adc82f4fd
AlenaKatt <katt.elena@yandex.ru>
2021-03-25 13:30:28 +0300

    Модуляция-повторно

---
+++ js/create-object.js
@@ -0,0 +1,52 @@
+import {randomInteger} from './util.js';
+import {MESSAGES_AUTHOR} from './data.js';
+import {NAMES_AUTHOR} from './data.js';
+import {COMMENT_INDEX} from './data.js';
+import {AVATAR_COUNT} from './data.js';
+import {LIKES_MIN} from './data.js';
+import {LIKES_MAX} from './data.js';
+import {OBJECTS_COUNT} from './data.js';
+
+const createComment = () => {
+  const randomIdCommentIndex = randomInteger (0, COMMENT_INDEX);
+  const randomAvatarIndex = randomInteger (1, AVATAR_COUNT);
+  const randomNameIndex = randomInteger (0, NAMES_AUTHOR.length - 1);
+
+  return {
+    id: randomIdCommentIndex,
+    avatar: 'img/avatar-' + randomAvatarIndex + '.svg',
+    message: getRandomMessages(),
+    name: NAMES_AUTHOR[randomNameIndex],
+  };
+};
+
+const getRandomMessages = () => {
+  // Получить рандомное число от 1 2 - количество элементов в массиве
+  const randomMessageCount =  randomInteger (1, 2);
+  // Получить рандомное число для комментариев - что именно
+  // Создать массив месаджей с количеством элементов, которое выпало Цикл
+  let arrComments = [];
+  for (let i = 0; i < randomMessageCount; i ++) {
+    const randomMessageComment = randomInteger (0, MESSAGES_AUTHOR.length - 1);
+    arrComments[i] = MESSAGES_AUTHOR[randomMessageComment];
+  }
+  // Объединить строки из массива
+  let arrString = arrComments.join(' ');
+  // Вернуть полученную строку
+  return arrString;
+}
+
+const createObject = (currentValue, index) => {
+  const randomLikesIndex = randomInteger (LIKES_MIN, LIKES_MAX);
+  return {
+    id: index + 1,
+    url: 'photos/' + (index + 1) + '.jpg',
+    description: 'котик',
+    likes: randomLikesIndex,
+    comments: createComment(),
+  }
+}
+
+const arrayObjects = new Array(OBJECTS_COUNT).fill(null).map((currentValue, index) => createObject(currentValue, index));
+
+arrayObjects();

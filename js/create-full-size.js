001cdf0bf4f73e2bfa9ff02a03a032eadf041550
AlenaKatt <katt.elena@yandex.ru>
2021-03-26 11:42:25 +0300

    Отрисуй меня полностью 2

--- js/create-full-size.js
+++ js/create-full-size.js
@@ -1,15 +1,22 @@
 import {createObject} from './create-object.js';
 
-const bigPicture = document.querySelector('.big-picture');
-//const createFullSise = createObject();
+const bigPictureContainer = document.querySelector('.big-picture');
+const pictureCommentCount = bigPictureContainer.querySelector('.social__comment-count');
+const pictureCommentsLoader = bigPictureContainer.querySelector('.comments-loader');
+const bodyModalOpen = document.querySelector('body');
 
-bigPicture.classList.remove('hidden');
+//bigPictureContainer.classList.remove('hidden');
+pictureCommentCount.classList.add('hidden');
+pictureCommentsLoader.classList.add('hidden');
+bodyModalOpen.classList.add('.modal-open');
 
-bigPicture.querySelector('.big-picture__img').src = url;
-bigPicture.querySelector('.likes-count').textContent = likes;
+const createFullSise = createObject();
+
+bigPictureContainer.querySelector('.big-picture__img').src = url;
+bigPictureContainer.querySelector('.likes-count').textContent = likes;
 //Количество комментариев comments подставьте как текстовое содержание элемента
-bigPicture.querySelector('.comments-count').textContent = comments;
+bigPictureContainer.querySelector('.comments-count').textContent = comments;
 //Список комментариев под фотографией: комментарии должны вставляться в блок .social__comments
-bigPicture.querySelector('.social__comments').textContent = comments;
-bigPicture.querySelector('.social__caption').textContent = description;
+bigPictureContainer.querySelector('.social__comments').textContent = comments;
+bigPictureContainer.querySelector('.social__caption').textContent = description;
 
--- js/create-miniature.js
+++ js/create-miniature.js
@@ -15,6 +15,3 @@ createMiniature.forEach(({url, likes, comments}) => {
 });
 
 miniatureContainer.appendChild(createMiniatureFragment);
-
-//Временно проверяю контейнер
-console.log(miniatureContainer);

9168a245e81cedc956401ae368f1a366ceab7c0f
AlenaKatt <katt.elena@yandex.ru>
2021-03-26 17:12:40 +0300

    Отрисуй меня полностью часть 1

--- js/create-miniature.js
+++ js/create-miniature.js
@@ -1,16 +1,18 @@
-import {arrayObjects} from './create-object.js';
+import {arrayObjects, createCommentCount} from './create-object.js';
 
 const miniatureContainer = document.querySelector('.pictures');
 const miniatureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const createMiniature = arrayObjects();
 const createMiniatureFragment = document.createDocumentFragment();
 
-createMiniature.forEach(({url, likes, comments}) => {
+createMiniature.forEach(({url, likes}) => {
   const miniatureElement = miniatureTemplate.cloneNode(true);
   miniatureElement.querySelector('.picture__img').src = url;
   miniatureElement.querySelector('.picture__likes').textContent = likes;
   //Количество комментариев comments подставьте как текстовое содержание элемента .picture__comments.
-  miniatureElement.querySelector('.picture__comments').textContent = comments;
+  //Узнать длину массива comments
+  //Вставить длину в 15 строку
+  miniatureElement.querySelector('.picture__comments').textContent = createCommentCount().length;
   createMiniatureFragment.appendChild(miniatureElement);
 });
 
--- js/create-object.js
+++ js/create-object.js
@@ -1,50 +1,59 @@
 import {randomInteger} from './util.js';
-import {MESSAGES_AUTHOR, NAMES_AUTHOR, COMMENT_INDEX, AVATAR_COUNT, LIKES_MIN, LIKES_MAX, OBJECTS_COUNT} from './data.js';
-
+import {COMMENT_COUNT_MAX, MESSAGES_AUTHOR, NAMES_AUTHOR, COMMENT_INDEX, AVATAR_COUNT, LIKES_MIN, LIKES_MAX, OBJECTS_COUNT} from './data.js';
+// Создать функцию
+// Внутри нее сделать цикл
+// Длину цикла определить рандомно
+// Это рандомное количество раз у меня должна отработать функция по созданию комментариев
+const createCommentCount = () => {
+  let arrCommentCount = [];
+  const randomCommentCount = randomInteger (1, COMMENT_COUNT_MAX);
+  for (let i = 0; i < randomCommentCount; i ++) {
+    arrCommentCount[i] = createComment();
+  }
+  return arrCommentCount;
+}
+// Функция по созданию комментария
 const createComment = () => {
   const randomIdCommentIndex = randomInteger (0, COMMENT_INDEX);
   const randomAvatarIndex = randomInteger (1, AVATAR_COUNT);
   const randomNameIndex = randomInteger (0, NAMES_AUTHOR.length - 1);
-
   return {
     id: randomIdCommentIndex,
     avatar: 'img/avatar-' + randomAvatarIndex + '.svg',
     message: getRandomMessages(),
     name: NAMES_AUTHOR[randomNameIndex],
-  };
-};
-
+  }
+}
+// Получить рандомное число от 1 2 - количество элементов в массиве
+// Получить рандомное число для комментариев - что именно
+// Создать массив месаджей с количеством элементов, которое выпало Цикл
+// Объединить строки из массива
+// Вернуть полученную строку
 const getRandomMessages = () => {
-  // Получить рандомное число от 1 2 - количество элементов в массиве
   const randomMessageCount =  randomInteger (1, 2);
-  // Получить рандомное число для комментариев - что именно
-  // Создать массив месаджей с количеством элементов, которое выпало Цикл
   let arrComments = [];
+  const randomMessageComment = randomInteger (0, MESSAGES_AUTHOR.length - 1);
   for (let i = 0; i < randomMessageCount; i ++) {
-    const randomMessageComment = randomInteger (0, MESSAGES_AUTHOR.length - 1);
     arrComments[i] = MESSAGES_AUTHOR[randomMessageComment];
   }
-  // Объединить строки из массива
   let arrString = arrComments.join(' ');
-  // Вернуть полученную строку
   return arrString;
 }
 
 const createObject = (currentValue, index) => {
   const randomLikesIndex = randomInteger (LIKES_MIN, LIKES_MAX);
-
   return {
     id: index + 1,
     url: 'photos/' + (index + 1) + '.jpg',
     description: 'котик',
     likes: randomLikesIndex,
-    comments: createComment(),
+    comments: createCommentCount(),
   }
 }
 
 const arrayObjects = () => new Array(OBJECTS_COUNT).fill(null).map((currentValue, index) => createObject(currentValue, index));
 
 export{
-  arrayObjects,
-  createObject
+  createCommentCount,
+  arrayObjects
 };
